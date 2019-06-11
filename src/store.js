39db8fc5e5455e0deb9c415fd2514713da41f9c5
Jack <shiungchenyu@gmail.com>
2019-06-11 17:59:02 +0800

    偵測裝置是否為 pc/ 移動裝置 vuex 裡存取

--- src/store.js
+++ src/store.js
@@ -5,12 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    deviceCheck: false // true => 移動裝置 || false => pc
   },
   mutations: {
-
+    DEVICEUPDATE (state, payload) {
+      state.deviceCheck = payload
+    }
   },
   actions: {
-
+    device_status (context, params) {
+      context.commit('DEVICEUPDATE', deviceMethods())
+    }
+  },
+  getters: {
+    deviceMobileStatus: state => state.deviceCheck
   }
 })
+
+function deviceMethods () {
+  // 判斷裝置
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
