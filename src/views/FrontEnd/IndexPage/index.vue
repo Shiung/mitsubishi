2f7f9af92433bca647a1fd8c664959417a262907
Jack <shiungchenyu@gmail.com>
2019-06-11 18:09:12 +0800

    1. 前台初始判斷裝置  2. snotify tag

--- src/views/FrontEnd/IndexPage/index.vue
+++ src/views/FrontEnd/IndexPage/index.vue
@@ -1,9 +1,18 @@
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'indexPage',
   components: {
     navItem: () => import('@/components/NavFront'),
     footerItem: () => import('@/components/FooterItem')
+  },
+  methods: {
+    // vuex
+    ...mapActions(['device_status'])
+  },
+  mounted () {
+    // 判斷裝置
+    this.device_status()
   }
 }
 </script>
