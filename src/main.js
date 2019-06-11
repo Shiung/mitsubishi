0f6bcfb9cbccf30f0c3554c14e659ed42f8b657b
Jack <shiungchenyu@gmail.com>
2019-06-11 17:57:52 +0800

    npm install img-lazy-load  snotify

--- src/main.js
+++ src/main.js
@@ -6,11 +6,31 @@ import store from './store'
 import 'bootstrap'
 // loading
 import Loading from 'vue-loading-overlay'
+// vue image loading
+import VueLazyload from 'vue-lazyload'
+// vue snotify
+import Snotify, { SnotifyPosition } from 'vue-snotify'
 
 Vue.config.productionTip = false
 
 // loading
 Vue.component('loading', Loading) // 全域使用
+// vue image loading
+Vue.use(VueLazyload, {
+  lazyComponent: true,
+  attempt: 1
+})
+// vue snotify
+const options = {
+  toast: {
+    position: SnotifyPosition.rightBottom,
+    timeout: 5000,
+    showProgressBar: false, // true,
+    closeOnClick: false,
+    pauseOnHover: true // false
+  }
+}
+Vue.use(Snotify, options)
 
 new Vue({
   router,
