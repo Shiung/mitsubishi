36d9aafbd96270f60995e3601c1b64c8075fe27f
Jack <shiungchenyu@gmail.com>
2019-06-11 18:40:41 +0800

    albumspage fix

--- src/views/FrontEnd/AlbumsPage/index.vue
+++ src/views/FrontEnd/AlbumsPage/index.vue
@@ -1,8 +1,49 @@
 <script>
+import { SnotifyPosition } from 'vue-snotify'
 export default {
   name: 'albumsPage',
   components: {
-    titleItem: () => import('@/components/TitleItem')
+    titleItem: () => import('@/components/TitleItem'),
+    albumsItem: () => import('@/components/AlbumsItem')
+  },
+  methods: {
+    deleteAlbumsGo (id) {
+      console.log(`刪除 ${id}`)
+      this.$snotify.confirm(`Delete Albums ID : ${id}`, `Are You Sure`, {
+        timeout: 0, // 沒有時間限制關閉
+        position: SnotifyPosition.centerCenter, // 位置
+        backdrop: 0.3, // 背景
+        buttons: [
+          {
+            text: 'Yes',
+            action: (toast) => {
+              this.removeRequest(id)
+              this.$snotify.remove(toast.id)
+            },
+            bold: true
+          },
+          {
+            text: 'No',
+            action: (toast) => {
+              this.$snotify.remove(toast.id)
+            },
+            bold: true
+          }
+        ]
+      })
+    },
+    editAlbumsGo (id) {
+      console.log(`編輯 ${id}`)
+    },
+    previewGo (id) {
+      console.log(`預覽 ${id}`)
+    },
+    printAndPurchaseGo (id) {
+      console.log(`購買 ${id}`)
+    },
+    removeRequest (id) {
+      console.log(`確認刪除 ${id}`)
+    }
   }
 }
 </script>
