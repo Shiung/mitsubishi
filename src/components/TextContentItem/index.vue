ae6c6997205b4893d2acded56b8066d943d316da
Jack <shiungchenyu@gmail.com>
2019-06-20 15:46:19 +0800

    文字編輯 初版

---
+++ src/components/TextContentItem/index.vue
@@ -0,0 +1,109 @@
+<script>
+export default {
+  name: 'textContentItem',
+  props: {
+    text: {
+      type: String,
+      default: null
+    },
+    position: {
+      type: String,
+      default: 'left'
+    },
+    weight: {
+      type: Number,
+      default: 400
+    },
+    scale: {
+      type: Number,
+      default: 1
+    },
+    color: {
+      type: String,
+      default: '#000'
+    },
+    standardWidth: {
+      type: Number,
+      default: 0
+    },
+    optionStatus: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      inputText: this.text,
+      textPosition: this.position,
+      // 文字編輯內容
+      standardWidthRatio: 1,
+      textScale: this.scale,
+      fontWeight: this.weight,
+      fontColor: this.color,
+      whRatio: 1, // 文字區域長寬比
+      showEdit: false
+    }
+  },
+  computed: {
+    // class 動態
+    stylePostion () {
+      return this.textPosition === 'left'
+        ? 'justify-content-start' : this.textPosition === 'center'
+          ? 'justify-content-center' : 'justify-content-end'
+    },
+    // style 動態
+    fontScale () {
+      let transformList = `scale(${this.textScale * this.standardWidthRatio})`
+      let transformPositon = this.textPosition
+      let fontWeightUse = this.fontWeight
+      let fontColor = this.fontColor
+      return {
+        transform: transformList,
+        transformOrigin: transformPositon,
+        fontWeight: fontWeightUse,
+        color: fontColor
+      }
+    },
+    textRatio () {
+      return this.$el.clientWidth
+    }
+  },
+  components: {
+    textEdit: () => import('@/components/TextEditItem')
+  },
+  methods: {
+    init () {
+      // 文字顯示寬長寬比
+      this.whRatio = this.$el.clientHeight / this.$el.clientWidth
+      // 文字比例
+      this.standardWidthRatio = this.$el.clientWidth / this.standardWidth
+      let vm = this
+      window.addEventListener('resize', function () {
+        vm.standardWidthRatio = vm.$el.clientWidth / vm.standardWidth
+      })
+    },
+    selectPostion (pos) {
+      this.textPosition = pos
+    },
+    edit () {
+      this.showEdit = true
+    },
+    updateText (obj) {
+      // 關閉視窗
+      this.showEdit = false
+      // 更新內容
+      this.inputText = obj.text
+      this.textPosition = obj.postion
+      this.fontWeight = obj.weight
+      this.textScale = obj.scale
+      this.fontColor = obj.color
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ src/components/TextEditItem/index.vue
@@ -0,0 +1,130 @@
+<script>
+import { Chrome } from 'vue-color'
+export default {
+  name: 'textEditItem',
+  props: {
+    textProp: {
+      type: String,
+      default: null
+    },
+    postionProp: {
+      type: String,
+      default: null
+    },
+    weightProp: {
+      type: Number,
+      default: 400
+    },
+    scaleProp: {
+      type: Number,
+      default: 1
+    },
+    colorProp: {
+      type: String,
+      default: '#000'
+    },
+    widthHeightRatio: {
+      type: Number,
+      default: 1
+    },
+    standardWidthProp: {
+      type: Number,
+      default: 890
+    }
+  },
+  data () {
+    return {
+      inputText: this.textProp,
+      textPosition: this.postionProp,
+      whRatio: this.widthHeightRatio,
+      // 文字編輯內容
+      inputEdit: null, // 新增編輯文字
+      fontStandard: 0, // 字體標準尺寸比 與layout 尺寸比較
+      fontWeight: this.weightProp, // font-weight
+      textScale: this.scaleProp, // 字體放大倍數
+      textColor: {
+        hex: this.colorProp
+      },
+      // 開關
+      scaleOpen: false,
+      inputOpen: false,
+      colorOpen: false
+    }
+  },
+  computed: {
+    // class 動態
+    stylePostion () {
+      return this.textPosition === 'left'
+        ? 'justify-content-start' : this.textPosition === 'center'
+          ? 'justify-content-center' : 'justify-content-end'
+    },
+    // style 動態
+    fontScale () {
+      let transformList = `scale(${this.textScale * this.fontStandard})`
+      let transformPositon = this.textPosition
+      let fontWeightUse = this.fontWeight
+      let fontColor = this.textColor.hex ? this.textColor.hex : '#000'
+      return {
+        transform: transformList,
+        transformOrigin: transformPositon,
+        fontWeight: fontWeightUse,
+        color: fontColor
+      }
+    }
+  },
+  components: {
+    'chrome-picker': Chrome
+  },
+  methods: {
+    init () {
+      // 初始設定
+      this.editAreaSize()
+      this.fontSize()
+      let vm = this
+      window.addEventListener('resize', function (e) {
+        if (this.innerWidth < 992) vm.fontSize()
+      })
+    },
+    fontSize () {
+      let editShowArea = this.$el.querySelector('.edit_area_father')
+      this.fontStandard = editShowArea.clientWidth / this.standardWidthProp
+    },
+    editAreaSize () {
+      let editShowArea = this.$el.querySelector('.edit_area_father')
+      editShowArea.style.width = `100%`
+      editShowArea.style.paddingTop = `${100 * this.whRatio}%`
+    },
+    inputChange () {
+      this.inputText = this.inputEdit
+      this.inputOpen = false
+    },
+    selectPostion (pos) {
+      this.textPosition = pos
+    },
+    cancel () {
+      this.$emit('closeHandler')
+    },
+    submit () {
+      let obj = {
+        text: this.inputText,
+        postion: this.textPosition,
+        weight: this.fontWeight,
+        color: this.textColor.hex,
+        scale: this.textScale
+      }
+      this.$emit('updateHandler', obj)
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  watch: {
+    inputOpen (val) {
+      if (!val) this.inputEdit = null
+    }
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
