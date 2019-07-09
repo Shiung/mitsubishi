efa8d64d2ee982a0bf2bc50526e977dec0dd3b1d
Jack <shiungchenyu@gmail.com>
2019-07-09 18:04:46 +0800

    scss utilty 改為 vue.config import

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/demo/'
-    : '/'
+    : '/',
+  // 全域sass變數
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "~@/assets/scss/customer/utilty";`
+      }
+    }
+  }
 }
