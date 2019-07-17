3998f9ad2241e7a4d5c418eba5f589d275dc14d8
Jack <shiungchenyu@gmail.com>
2019-07-17 18:27:52 +0800

    假資料 不用 json-server

--- src/views/FrontEnd/EditAlbumPage/index.vue
+++ src/views/FrontEnd/EditAlbumPage/index.vue
@@ -39,13 +39,20 @@ export default {
     },
     getFullList () {
       this.opened = false
-      return this.axios.get(`${process.env.VUE_APP_APIPATH}/albums`)
-        .then(res => {
-          this.albumSelectID = res.data[0].id
-          this.albumsApi = res.data
+      // return this.axios.get(`${process.env.VUE_APP_APIPATH}/albums`)
+      //   .then(res => {
+      //     this.albumSelectID = res.data[0].id
+      //     this.albumsApi = res.data
+      //     this.opened = true
+      //   })
+      //   .catch(e => console.log(e))
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          this.albumSelectID = testData[0].id
+          this.albumsApi = testData
           this.opened = true
         })
-        .catch(e => console.log(e))
+      })
     },
     page (p) {
       this.$router.push({ name: 'editAlbumPage', query: { id: p } })
@@ -131,6 +138,119 @@ export default {
     }
   }
 }
+
+// 假資料
+var testData = [
+  {
+    'id': 'album_1',
+    'width': 842,
+    'height': 595,
+    'layers': [
+      {
+        'id': 'img_1',
+        'x': 10,
+        'y': 10,
+        'w': 406,
+        'h': 282.5,
+        'type': 'img',
+        'img': {
+          'src': 'https://www.tgilive.com/image/frontend/banner/A9BN-01.jpg'
+        },
+        'translate': {
+          'x': 0.32727272727272655,
+          'y': 0.06862745098039225
+        },
+        'rotate': 0,
+        'scale': 1.07025
+      },
+      {
+        'id': 'img_2',
+        'x': 426,
+        'y': 10,
+        'w': 406,
+        'h': 282.5,
+        'type': 'img'
+      },
+      {
+        'id': 'img_3',
+        'x': 10,
+        'y': 302.5,
+        'w': 822,
+        'h': 200,
+        'type': 'img',
+        'img': {
+          'src': 'https://www.tgilive.com/image/frontend/banner/B9BN-02.jpg'
+        },
+        'translate': {
+          'x': 0.01910112359550554,
+          'y': -0.004608294930875619
+        },
+        'scale': 2.08124
+      },
+      {
+        'id': 'text_1',
+        'x': 10,
+        'y': 512.5,
+        'w': 822,
+        'h': 73,
+        'type': 'text',
+        'position': 'right',
+        'color': '#00f',
+        'scale': 1.5,
+        'weight': 400,
+        'textVal': '彼らの機器や装置はすべて生命体だ。彼らの機器や装置はすべて生命体だ'
+      }
+    ]
+  },
+  {
+    'id': 'album_2',
+    'width': 842,
+    'height': 595,
+    'layers': [
+      {
+        'id': 'img_1',
+        'x': 10,
+        'y': 10,
+        'w': 406,
+        'h': 282.5,
+        'type': 'img'
+      },
+      {
+        'id': 'img_2',
+        'x': 426,
+        'y': 10,
+        'w': 406,
+        'h': 282.5,
+        'type': 'img',
+        'img': {
+          'src': 'https://www.tgilive.com/image/product/1230_B42SPL000TBO_img1.jpg'
+        },
+        'translate': {
+          'x': 0.32727272727272655,
+          'y': 0.06862745098039225
+        },
+        'rotate': 0,
+        'scale': 1.07025
+      },
+      {
+        'id': 'img_3',
+        'x': 10,
+        'y': 302.5,
+        'w': 822,
+        'h': 283.5,
+        'type': 'img',
+        'img': {
+          'src': 'https://www.tgilive.com/image/frontend/banner/C8BN-03.jpg'
+        },
+        'translate': {
+          'x': -0.026732209737827792,
+          'y': 0.4532230303703284
+        },
+        'scale': 2.08124
+      }
+    ]
+  }
+]
 </script>
 
 <template src="./template.html"></template>
