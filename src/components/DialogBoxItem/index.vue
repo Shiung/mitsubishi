dc90dc76ce94d9c7d76c3c65b1a4fb65b48ccb0f
Jack <shiungchenyu@gmail.com>
2019-07-17 18:28:35 +0800

    dialogBox 跳視窗確認 元件

---
+++ src/components/DialogBoxItem/index.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: 'dialogBoxItem',
+  props: {
+    idProps: {
+      type: Number,
+      required: true
+    },
+    typeProps: {
+      type: Number, // 0: delete 1: confirm 2: print& Purchase
+      required: true
+    },
+    titleProps: {
+      type: String,
+      default: 'this is title'
+    },
+    subTitleProps: {
+      type: String,
+      default: 'this is sub title'
+    },
+    describeProps: {
+      type: String,
+      default: '印PされたアルバムH S HIの担当者によCD審査され、誰かがあ Eたに連絡しIA。'
+    }
+  },
+  data () {
+    return {
+      showBox: false
+    }
+  },
+  computed: {
+    confirmBtnStr () {
+      return this.typeProps === 0 ? '刪除 delete' : '確認 confirm'
+    }
+  },
+  methods: {
+    confirm () {
+      this.$emit('confrimEmit', {
+        id: this.idProps,
+        type: this.typeProps
+      })
+    },
+    cancel () {
+      this.$emit('cancelEmit')
+    }
+  },
+  mounted () {
+    this.showBox = true
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
--- src/views/FrontEnd/AlbumsPage/index.vue
+++ src/views/FrontEnd/AlbumsPage/index.vue
@@ -1,36 +1,54 @@
 <script>
-import { SnotifyPosition } from 'vue-snotify'
+// import { SnotifyPosition } from 'vue-snotify'
 export default {
   name: 'albumsPage',
+  data () {
+    return {
+      // dialogBox提示視窗
+      dialogStatus: false,
+      dialogID: '',
+      dialogType: '', // 0: delete 1: confirm 2: print& Purchase
+      dialogTitle: '',
+      dialogSubTitle: '',
+      dialogDescibe: ''
+    }
+  },
   components: {
     titleItem: () => import('@/components/TitleItem'),
-    albumsItem: () => import('@/components/AlbumsItem')
+    albumsItem: () => import('@/components/AlbumsItem'),
+    dialogBox: () => import('@/components/DialogBoxItem')
   },
   methods: {
     deleteAlbumsGo (id) {
       console.log(`刪除 ${id}`)
-      this.$snotify.confirm(`Delete Albums ID : ${id}`, `Are You Sure`, {
-        timeout: 0, // 沒有時間限制關閉
-        position: SnotifyPosition.centerCenter, // 位置
-        backdrop: 0.3, // 背景
-        buttons: [
-          {
-            text: 'Yes',
-            action: (toast) => {
-              this.removeRequest(id)
-              this.$snotify.remove(toast.id)
-            },
-            bold: true
-          },
-          {
-            text: 'No',
-            action: (toast) => {
-              this.$snotify.remove(toast.id)
-            },
-            bold: true
-          }
-        ]
-      })
+      // this.$snotify.confirm(`Delete Albums ID : ${id}`, `Are You Sure`, {
+      //   timeout: 0, // 沒有時間限制關閉
+      //   position: SnotifyPosition.centerCenter, // 位置
+      //   backdrop: 0.3, // 背景
+      //   buttons: [
+      //     {
+      //       text: 'Yes',
+      //       action: (toast) => {
+      //         this.removeRequest(id)
+      //         this.$snotify.remove(toast.id)
+      //       },
+      //       bold: true
+      //     },
+      //     {
+      //       text: 'No',
+      //       action: (toast) => {
+      //         this.$snotify.remove(toast.id)
+      //       },
+      //       bold: true
+      //     }
+      //   ]
+      // })
+      this.dialogID = id
+      this.dialogType = 0
+      this.dialogTitle = '是否刪除相簿'
+      this.dialogSubTitle = ''
+      this.dialogDescibe = ''
+      this.dialogStatus = true
     },
     editAlbumsGo (id) {
       console.log(`編輯 ${id}`)
@@ -40,10 +58,30 @@ export default {
       console.log(`預覽 ${id}`)
     },
     printAndPurchaseGo (id) {
-      console.log(`購買 ${id}`)
+      // console.log(`購買 ${id}`)
+      this.dialogID = id
+      this.dialogType = 2
+      this.dialogTitle = '是否列印相簿'
+      this.dialogSubTitle = '列印相簿會由ASAHI人員審查後，專人與您聯絡'
+      this.dialogDescibe = '印PされたアルバムH S HIの担当者によCD審査され、誰かがあ Eたに連絡しIA。'
+      this.dialogStatus = true
     },
     removeRequest (id) {
       console.log(`確認刪除 ${id}`)
+    },
+    // dialogBox跳窗
+    confirmHandler (obj) {
+      // type 0: delete 1: confirm 2: print& Purchase
+      console.log(`ID: ${obj.id} , 類別:${obj.type === 0 ? '刪除' : obj.type === 1 ? '確認' : '列印'}`)
+      this.closeDialogBox()
+    },
+    closeDialogBox () {
+      this.dialogStatus = false
+      this.dialogID = null
+      this.dialogType = null
+      this.dialogTitle = null
+      this.dialogSubTitle = null
+      this.dialogDescibe = null
     }
   }
 }
