f562da363e9deb3e47342bb4dbab184b46febe52
Jack <shiungchenyu@gmail.com>
2019-08-01 16:43:44 +0800

    後台登入 權限 登入頁面 / 主頁

--- src/views/BackEnd/IndexPage/index.vue
+++ src/views/BackEnd/IndexPage/index.vue
@@ -1,7 +1,15 @@
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'indexPage',
+  data () {
+    return {
+      apiPath: `${process.env.VUE_APP_APIPATH}/admin`
+    }
+  },
   computed: {
+    // vuex
+    ...mapGetters('adminStore', ['adminToken', 'adminCookieKey', 'adminInfo']),
     routerName () {
       return this.$route.name
     },
@@ -39,6 +47,50 @@ export default {
   components: {
     navItem: () => import('@/components/NavBack'),
     sideBarItem: () => import('@/components/SideBarBack')
+  },
+  methods: {
+    // vuex
+    ...mapActions('adminStore', ['token_update', 'log_out', 'set_adminInfo']),
+    async initToken () {
+      let localToken = this.getlocalStorageStatus()
+      if (!localToken) {
+        this.$router.push({ name: 'adminLogin' })
+      } else {
+        await this.getAdminInfo(localToken)
+      }
+    },
+    getAdminInfo (token) {
+      let url = this.apiPath
+      return this.axios.get(url, {
+        headers: {
+          'Authorization': token
+        }
+      })
+        .then(res => {
+          let respond = res.data.data
+          this.set_adminInfo(respond)
+          !this.adminToken && (this.token_update(token))
+        })
+        .catch(e => {
+          // 登入失敗(token 過期...)
+          // console.log('token 過期')
+          this.log_out()
+          this.$router.push({ name: 'adminLogin' })
+        })
+    },
+    getlocalStorageStatus () {
+      let key = this.adminCookieKey
+      return localStorage[key] ? localStorage[key] : null
+    }
+  },
+  created () {
+    // 1. 登入進來
+    if (this.adminToken) return
+    // 2. 登入初始
+    this.initToken()
+  },
+  watch: {
+    adminToken (val) { !val && (this.$router.push({ name: 'adminLogin' })) }
   },
   beforeRouteUpdate (to, from, next) {
     // console.log(to)
--- src/views/BackEnd/Login/index.vue
+++ src/views/BackEnd/Login/index.vue
@@ -1,6 +1,63 @@
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      account: '',
+      password: '',
+      // loading
+      showLoading: false,
+      loadingOpacity: 0.8,
+      fullNone: true,
+      apiPath: `${process.env.VUE_APP_APIPATH}/login`
+    }
+  },
+  computed: {
+    // vuex
+    ...mapGetters('adminStore', ['adminToken', 'adminCookieKey', 'adminInfo'])
+  },
+  methods: {
+    // vuex
+    ...mapActions('adminStore', ['token_update', 'log_out', 'set_adminInfo']),
+    getlocalStorageStatus () {
+      let key = this.adminCookieKey
+      return localStorage[key] ? localStorage[key] : null
+    },
+    loginHandler () {
+      this.$validator.validate().then(result => {
+        if (result) {
+          this.loginRequest()
+        }
+      })
+    },
+    async loginRequest () {
+      this.showLoading = true
+      await this.loginAPI()
+      this.showLoading = false
+      if (this.adminToken) this.$router.push({ name: 'admin' })
+    },
+    loginAPI () {
+      let url = this.apiPath
+      let data = {
+        email: this.account,
+        password: this.password
+      }
+      return this.axios.post(url, data)
+        .then(res => {
+          let respond = res.data.data
+          this.token_update(`${respond.token_type} ${respond.access_token}`)
+          this.set_adminInfo(respond.admin)
+        })
+        .catch(e => {
+          // console.log('結果有 error')
+          // console.error(e)
+        })
+    }
+  },
+  created () {
+    if (this.getlocalStorageStatus()) this.$router.push({ name: 'admin' })
+  }
 }
 </script>
 
